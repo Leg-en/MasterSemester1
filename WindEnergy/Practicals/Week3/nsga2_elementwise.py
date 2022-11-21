549d9257aea6421074eb04207ac8941e8f177e34
Emily Sterthaus <e.sterthaus@outlook.de>
2022-11-21 11:41:47 +0100

    code cleanup, mp added

--- WindEnergy/Practicals/Week3/nsga2_elementwise.py
+++ WindEnergy/Practicals/Week3/nsga2_elementwise.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import os
 import pickle
 from itertools import combinations
@@ -6,7 +7,8 @@ import geopandas as geop
 import matplotlib.pyplot as plt
 import numpy as np
 from pymoo.algorithms.moo.nsga2 import NSGA2
-from pymoo.core.problem import Problem
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.core.problem import StarmapParallelization
 from pymoo.operators.crossover.pntx import TwoPointCrossover
 from pymoo.operators.mutation.bitflip import BitflipMutation
 from pymoo.operators.sampling.rnd import BinaryRandomSampling
@@ -53,42 +55,37 @@ except FileNotFoundError:
 
 # Die distanzmatrix enthält jetzt alle relevanten distanz informationen
 
-class WindEnergySiteSelectionProblem(Problem):
+class WindEnergySiteSelectionProblem(ElementwiseProblem):
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         # super().__init__(n_var=gdf_optimization.shape[0], n_obj=2, n_ieq_constr=0, xl=0.0, xu=1.0)
         super().__init__(n_var=len(gdf_np), n_obj=2, n_ieq_constr=0, xl=0.0,
-                         xu=1.0)  # Bearbeitet weil v_var nicht mehr gepasst hat
+                         xu=1.0, **kwargs)  # Bearbeitet weil v_var nicht mehr gepasst hat
 
     def _evaluate(self, x, out, *args, **kwargs):
         DISTANCE_THRESHOLD = 4000
 
-        def correct(x1, y1, y2):
+        def correct(y1, y2):
             # Statisch immer das erste element auf False setzen
             # Todo: Durch was sinnvolles ersetzen
-            x[x1, y1] = False
-            return y
+            x[y1] = False
+            return y1
 
-        def regenerate(val):
-            indices = np.where(x[val, :])
-            combs = combinations(indices[0], 2)
-            corrected = np.zeros((0))
-            for item in combs:
-                if distance_matrix[item[0], item[1]] < DISTANCE_THRESHOLD and item[0] not in corrected and item[
-                    1] not in corrected:
-                    c_val = correct(val, item[0], item[1])
-                    corrected = np.append(corrected, c_val)
-
-        for z in tqdm(range(len(x))):
-            regenerate(z)
-
+        indices = np.where(x)
+        combs = combinations(indices[0], 2)
+        corrected = np.zeros((0))
+        for item in combs:
+            if distance_matrix[item[0], item[1]] < DISTANCE_THRESHOLD and item[0] not in corrected and \
+                    item[1] not in corrected:
+                c_val = correct(item[0], item[1])
+                corrected = np.append(corrected, c_val)
 
         abstand_such = np.where(x, gdf_np[:, 0], 0)
-        abstand_summe = np.sum(abstand_such, axis=1)
+        abstand_summe = np.sum(abstand_such)
         # for 2 objectives: out["F"] = np.column_stack([f1, f2])
 
         energie_such = np.where(x, gdf_np[:, 1], 0)
-        energie_summe = np.sum(energie_such, axis=1)
+        energie_summe = np.sum(energie_such)
         energie_summe_corrected = energie_summe * -1
 
         out["F"] = np.column_stack([abstand_summe, energie_summe_corrected])
@@ -96,50 +93,58 @@ class WindEnergySiteSelectionProblem(Problem):
         # out["G"] = constraints_np
 
 
-algorithm = NSGA2(pop_size=10,
-                  sampling=BinaryRandomSampling(),
-                  crossover=TwoPointCrossover(),
-                  mutation=BitflipMutation(),
-                  eliminate_duplicates=True)
+def main():
+    algorithm = NSGA2(pop_size=100,
+                      sampling=BinaryRandomSampling(),
+                      crossover=TwoPointCrossover(),
+                      mutation=BitflipMutation(),
+                      eliminate_duplicates=True)
 
-problem = WindEnergySiteSelectionProblem()
+    n_proccess = 8
+    pool = multiprocessing.Pool(n_proccess)
+    runner = StarmapParallelization(pool.starmap)
 
-res = minimize(problem,
-               algorithm,
-               ('n_gen', 100),
-               seed=1,
-               verbose=True,
-               save_history=True)
+    problem = WindEnergySiteSelectionProblem(elementwise_runner=runner)
 
-with open("result2.pkl", "wb") as out:
-    pickle.dump(res, out, pickle.HIGHEST_PROTOCOL)
+    res = minimize(problem,
+                   algorithm,
+                   ('n_gen', 100),
+                   seed=1,
+                   verbose=True)
 
-fitness_vals = []
+    with open("result2.pkl", "wb") as out:
+        pickle.dump(res, out, pickle.HIGHEST_PROTOCOL)
 
-for iteration in res.history:
-    x = []
-    y = []
-    for item in iteration.off:
-        x.append(item.F[0])
-        y.append(item.F[1])
-    fitness_vals.append([x, y])
-np_fitness = np.asarray(fitness_vals)
+    fitness_vals = []
 
-# Manual Scotter
-plot_val = [1, 10, 30, 50]
-for i in plot_val:
-    plt.scatter(np_fitness[i, 0], np_fitness[i, 1])
-plt.scatter(res.F[:, 0], res.F[:, 1])
-plt.show()
+    for iteration in res.history:
+        x = []
+        y = []
+        for item in iteration.off:
+            x.append(item.F[0])
+            y.append(item.F[1])
+        fitness_vals.append([x, y])
+    np_fitness = np.asarray(fitness_vals)
 
-# Konvergenz plot von Pymoo
-# n_evals = np.array([e.evaluator.n_eval for e in res.history])
-# opt = np.array([e.opt[0].F for e in res.history])
-#
-# plt.title("Convergence")
-# plt.plot(n_evals, opt, "--")
-# plt.yscale("log")
-# plt.show()
+    # Manual Scotter
+    plot_val = [1, 10, 30, 50]
+    for i in plot_val:
+        plt.scatter(np_fitness[i, 0], np_fitness[i, 1])
+    plt.scatter(res.F[:, 0], res.F[:, 1])
+    plt.show()
 
-# Pymoo scatter
-# Scatter().add(res.F).show()
+    # Konvergenz plot von Pymoo
+    # n_evals = np.array([e.evaluator.n_eval for e in res.history])
+    # opt = np.array([e.opt[0].F for e in res.history])
+    #
+    # plt.title("Convergence")
+    # plt.plot(n_evals, opt, "--")
+    # plt.yscale("log")
+    # plt.show()
+
+    # Pymoo scatter
+    # Scatter().add(res.F).show()
+
+
+if __name__ == "__main__":
+    main()
