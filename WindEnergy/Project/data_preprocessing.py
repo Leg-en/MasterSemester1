3d7fc7ca80e3cc19eb5c71d198001415bee5f834
Emily Sterthaus <e.sterthaus@outlook.de>
2022-11-26 10:12:34 +0100

    data_preprocessing.py fishnet added

--- WindEnergy/Project/data_preprocessing.py
+++ WindEnergy/Project/data_preprocessing.py
@@ -1,5 +1,7 @@
 import arcpy
+import shapefile
 from arcpy import analysis
+from arcpy import management
 
 arcpy.env.workspace = r"C:\workspace\MasterSemester1\WindEnergy\Project\data\study area"
 arcpy.env.parallelProcessingFactor = "100%"
@@ -33,3 +35,26 @@ analysis.PairwiseIntersect(bestehend_buffered, buffered_intersects, "ALL", None,
 
 Intersects_erased = processed_data + r"\Intersects_erased"
 analysis.PairwiseErase(Area_Flurstuecke_bufferd, buffered_intersects, Intersects_erased, None)
+
+# Points über das gesamte ausmaß ansetzen und dann entsprechend Clippend. Deutlich Simpler, stärker Limitiert. Daher grobere zellgröße
+
+sf = shapefile.Reader(Intersects_erased)
+originCoordinate = "" + str(sf.bbox[0]) + " " + str(sf.bbox[1])
+yAxisCoordinate = "" + str(sf.bbox[0]) + " " + str(sf.bbox[1] + 10)
+oppositeCoorner = "" + str(sf.bbox[2]) + " " + str(sf.bbox[3])
+cell_size = 5
+labels = 'LABELS'
+# Extent is set by origin and opposite corner - no need to use a template fc
+templateExtent = '#'
+geometryType = 'POLYLINE'
+
+points = processed_data + r"\points"
+management.CreateFishnet(points, originCoordinate, yAxisCoordinate, cell_size, cell_size, None, None, oppositeCoorner,
+                         labels, templateExtent, geometryType)
+
+intersection_points = processed_data + r"\inter_points"
+analysis.PairwiseClip(points + "_label", Intersects_erased,
+                      intersection_points,
+                      None)
+
+# Todo: Fundamente mit einbeziehen, Abstände zu gebäuden mit einbeziehen
